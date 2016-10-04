3998d8f0341423f4c5eef34151588192a9c388b2
Nate Jarvis <njarvis@unm.edu>
2016-10-03 18:33:20 -0600

    new validation syntax

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
   #belongs_to :user
-  validates_presence_of :post_id
-  validates_presence_of :body
+  validates :post_id, presence: true
+  validates :body, presence: true
 end
